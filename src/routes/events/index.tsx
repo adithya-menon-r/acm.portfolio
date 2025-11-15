63673d130804e2b847104b44123df4b44e06adb5
cootot <karthikeyanpuranamgopalakrishn@gmail.com>
2025-11-16 02:33:12 +0530

    feat: improve EventCard mobile responsiveness,refine hover interactions and reduce extra padding

--- src/routes/events/index.tsx
+++ src/routes/events/index.tsx
@@ -10,13 +10,15 @@ export const Route = createFileRoute('/events/')({
 
 
 function RouteComponent() {
-  const events = Object.entries(eventDb)
-
+  const events = Object.entries(eventDb).sort(
+  ([, eventA], [, eventB]) =>
+    eventB.sortDate.localeCompare(eventA.sortDate)
+);
   return (
     <div className="bg-white dark:bg-zinc-900 text-zinc-900 dark:text-white min-h-screen font-sans">
       <div className="container p-4 mx-auto md:p-8">
         <section className="pt-2 pb-10 text-center md:pt-4 md:pb-14">
-          <h1 className="text-3xl font-extrabold tracking-wide md:text-3xl text-zinc-700 dark:text-zinc-300">
+          <h1 className="text-3xl font-extrabold tracking-wide md:text-5xl text-zinc-700 dark:text-zinc-300">
             Our Past Events
           </h1>
         </section>
