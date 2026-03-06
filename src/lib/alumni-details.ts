a43811c64a8e672a8261b3c8664d3bbe3f628da3
Adithya Menon R <adukottan@gmail.com>
2026-03-06 21:26:51 +0530

    fix: compressed alumni images

--- src/lib/alumni-details.ts
+++ src/lib/alumni-details.ts
@@ -15,27 +15,27 @@ export const AlumniData: AlumniYear[] = [
     members: [
       {
         name: "Sneha S S",
-        photo: "/alumni/2020-21/sneha sridhar1.png",
+        photo: "/alumni/2020-21/sneha-sridhar.png",
         title: "Chairperson",
       },
       {
         name: "Sreeramji K S",
-        photo: "/alumni/2020-21/sreeramji k s.jpg",
+        photo: "/alumni/2020-21/sreeramji-ks.jpg",
         title: "Vice Chairperson",
       },
       {
         name: "Deepika J",
-        photo: "/alumni/2020-21/deepika.jpg",
+        photo: "/alumni/2020-21/deepika-j.jpg",
         title: "Secretary",
       },
       {
         name: "Hemanth Duddu",
-        photo: "/alumni/2020-21/hemanth duddu1.jpg",
+        photo: "/alumni/2020-21/hemanth-duddu.jpg",
         title: "Treasurer",
       },
       {
-        name: "Malavika",
-        photo: "/alumni/2020-21/malavika.jpg",
+        name: "Malavika M",
+        photo: "/alumni/2020-21/malavika-m.jpg",
         title: "Web Master",
       },
     ],
@@ -45,32 +45,32 @@ export const AlumniData: AlumniYear[] = [
     members: [
       {
         name: "Jagadeeshram D",
-        photo: "/alumni/2021-22/jagadeeshram d.jpg",
+        photo: "/alumni/2021-22/jagadeeshram-d.jpg",
         title: "Chairperson",
       },
       {
         name: "Adithi Narayan",
-        photo: "/alumni/2021-22/adithi narayan.jpg",
+        photo: "/alumni/2021-22/adithi-narayan.jpg",
         title: "Vice Chairperson",
       },
       {
         name: "Harish K",
-        photo: "/alumni/2021-22/harish k.jpg",
+        photo: "/alumni/2021-22/harish-k.jpg",
         title: "Secretary",
       },
       {
         name: "Hitharth Kadam",
-        photo: "/alumni/2021-22/hitharth kadam.jpeg",
+        photo: "/alumni/2021-22/hitharth-kadam.jpeg",
         title: "Membership Chairperson",
       },
       {
         name: "Parripati Divyasri",
-        photo: "/alumni/2021-22/divyasri parripati.jpg",
+        photo: "/alumni/2021-22/divyasri-parripati.jpg",
         title: "Treasurer",
       },
       {
         name: "Abhinay Potti",
-        photo: "/alumni/2021-22/abhinay potti.jpg",
+        photo: "/alumni/2021-22/abhinay-potti.jpg",
         title: "Web Master",
       },
     ],
@@ -94,8 +94,8 @@ export const AlumniData: AlumniYear[] = [
         title: "Secretary",
       },
       {
-        name: "Gomathi",
-        photo: "/alumni/2022-23/gomathi.jpg",
+        name: "Gomathi R",
+        photo: "/alumni/2022-23/gomathi-r.jpg",
         title: "Membership Chairperson",
       },
       {
@@ -115,32 +115,32 @@ export const AlumniData: AlumniYear[] = [
     members: [
       {
         name: "Kishore S",
-        photo: "/alumni/2023-24/kishore_s.jpg",
+        photo: "/alumni/2023-24/kishore-s.jpg",
         title: "Chairperson",
       },
       {
         name: "Duvvuri Ananya",
-        photo: "/alumni/2023-24/ananya_duvvuri.jpg",
+        photo: "/alumni/2023-24/ananya-duvvuri.jpg",
         title: "Vice Chairperson",
       },
       {
         name: "Pradeep Karthik M",
-        photo: "/alumni/2023-24/pradeep_karthik.jpg",
+        photo: "/alumni/2023-24/pradeep-karthik.jpg",
         title: "Secretary",
       },
       {
         name: "Akash S",
-        photo: "/alumni/2023-24/akash_s.jpg",
+        photo: "/alumni/2023-24/akash-s.jpg",
         title: "Membership Chairperson",
       },
       {
         name: "Aparna A",
-        photo: "/alumni/2023-24/aparna_a1.jpg",
+        photo: "/alumni/2023-24/aparna-a.jpg",
         title: "Treasurer",
       },
       {
         name: "Praveen Kumar M",
-        photo: "/alumni/2023-24/praveen_kumar.jpg",
+        photo: "/alumni/2023-24/praveen-kumar.jpg",
         title: "Web Master",
       },
     ],
@@ -150,7 +150,7 @@ export const AlumniData: AlumniYear[] = [
     members: [
       {
         name: "Ashwin Narayanan S",
-        photo: "/alumni/2024-25/ashwin_narayanan_s.jpg",
+        photo: "/alumni/2024-25/ashwin-narayanan-s.jpg",
         title: "Chairperson",
       },
       {
@@ -160,7 +160,7 @@ export const AlumniData: AlumniYear[] = [
       },
       {
         name: "Abhinav Ramakrishnan",
-        photo: "/alumni/2024-25/abhinav_r.jpg",
+        photo: "/alumni/2024-25/abhinav-r.jpg",
         title: "Secretary",
       },
       {
@@ -170,7 +170,7 @@ export const AlumniData: AlumniYear[] = [
       },
       {
         name: "Aadithyaa Eeswaran",
-        photo: "/alumni/2024-25/aadhitya.jpg",
+        photo: "/alumni/2024-25/aadhitya-e.jpg",
         title: "Web Master",
       },
     ],
@@ -183,25 +183,26 @@ export const AlumniData: AlumniYear[] = [
   //       name: "Aadithyaa Eeswaran",
   //       photo: "/alumni/2025-26/aadhitya.jpg",
   //       title: "Chairperson",
-  //      
   //     },
   //     {
   //       name: "Leela Mahalakshmi Marupuru",
   //       photo: "/alumni/2025-26/leela-m.jpg",
   //       title: "Vice Chairperson",
-  //       
   //     },
   //     {
   //       name: "Sarath Chandra",
   //       photo: "/alumni/2025-26/sarath-chandra.jpg",
   //       title: "Secretary",
-  //       
   //     },
   //     {
   //       name: "Sanyam Bhardwaj",
   //       photo: "/alumni/2025-26/sanyam.jpg",
   //       title: "Treasurer",
-  //      
+  //     },
+  //     {
+  //       name: "Adithya Menon R",
+  //       photo: "/alumni/2025-26/adithya-menon-r.jpg",
+  //       title: "Web Master",
   //     },
   //   ],
   // },
